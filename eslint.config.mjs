744d7e282a930dc64920d65bff03ee8dabd06324
abdel0233 <abdelahzeroual5@gmail.com>
2026-03-16 18:52:47 +0100

    fix: simplify eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  {
+    rules: {},
+  },
   globalIgnores([
     ".next/**",
     "out/**",
